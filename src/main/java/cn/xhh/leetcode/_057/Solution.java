55e279e58e66d7eb88c2e0e9e642e50f9a94d279
tiger <bigtiger02@gmail.com>
2020-11-05 20:50:23 +0800

    change solution

--- src/main/java/cn/xhh/leetcode/_057/Solution.java
+++ src/main/java/cn/xhh/leetcode/_057/Solution.java
@@ -13,57 +13,30 @@ public class Solution {
 
     public int[][] insert(int[][] intervals, int[] newInterval) {
         Stack<Integer> stack = new Stack<>();
-        List<Integer[]> list = new LinkedList<>();
-        int t = 0, index = 0;
-        while(t < intervals.length * 2){
-            int i = t/2,j = t%2;
-            //入栈
+        int[][] results = new int[intervals.length+1][2];
+        int index = 0;
+        for (int i = 0; i < intervals.length; i++) {
             if(stack.isEmpty()){
-                if(index < 2){
-                    if(intervals[i][j] > newInterval[index]){
-                        stack.push(newInterval[index++]);
-                    }else if(intervals[i][j] < newInterval[index]){
-                        stack.push(intervals[i][j]);
-                        t++;
-                    }else {
-                        stack.push(newInterval[index++]);
-                        t++;
-                    }
+                if(intervals[i][1] < newInterval[0]){
+                    results[index++] = intervals[i];
+                } else if(intervals[i][0] <= newInterval[0]){
+                    stack.push(intervals[i][0]);
                 }else{
-                    stack.push(intervals[i][j]);
-                    t++;
+                    if(newInterval[1] < intervals[i][0]){
+
+                    }
+                    stack.push(newInterval[0]);
                 }
-            }else{
-                if(index < 2){
-                    if(intervals[i][j] < newInterval[index]){
-                        list.add(new Integer[]{stack.pop(), newInterval[index++]});
-                        t++;
-                    }else if(intervals[i][j] > newInterval[index]){
-                        list.add(new Integer[]{stack.pop(), intervals[i][j]});
-                        t++;
-                        index++;
-                    }else {
-                        t++;
-                    }
+            }else {
+                if(intervals[i][0] > newInterval[1]){
+                    results[index++] = new int[]{stack.pop(), newInterval[1]};
+                    results[index++] = intervals[i];
                 }else{
-                    if(j == 0){
-                        list.add(new Integer[]{stack.pop(), intervals[i][j+1]});
-                        t+=2;
-                    }else {
-                        list.add(new Integer[]{stack.pop(), intervals[i][j]});
-                        t++;
-                    }
+                    results[index++] = new int[]{stack.pop(), intervals[i][1]};
                 }
             }
         }
-        
-        int[][] results = new int[list.size()][2];
-        for (int i = 0; i < list.size(); i++) {
-            Integer[] tmp = list.get(i);
-            System.out.println(tmp[0] + "," + tmp[1]);
-            results[i][0] = tmp[0];
-            results[i][1] = tmp[1];
-        }
-        return results;
+
+        return Arrays.copyOfRange(results, 0 ,index);
     }
 }
