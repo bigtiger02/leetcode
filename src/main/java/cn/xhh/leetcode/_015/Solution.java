866cf8ffe41e9a3c44a58a5bb13ec3084a98b031
tiger <bigtiger02@gmail.com>
2020-11-16 01:16:03 +0800

    solve 3sum

---
+++ src/main/java/cn/xhh/leetcode/_015/Solution.java
@@ -0,0 +1,50 @@
+package cn.xhh.leetcode._015;
+
+import java.util.*;
+
+/**
+ * 3Sum
+ * <a href="https://leetcode-cn.com/problems/3sum/">3sum</a>
+ * @author <a href="mailto:bigtiger02@gmail.com">xhh</a>
+ * @date 2020/11/15
+ */
+public class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        if(null == nums || nums.length < 3){
+            return new ArrayList<>();
+        }
+
+        Arrays.sort(nums);
+        if(nums[0] > 0 || nums[nums.length-1] < 0){
+            return new ArrayList<>();
+        }
+
+        List<List<Integer>> results = new ArrayList<>();
+        for (int first = 0; first < nums.length; first++) {
+            if(first > 0 && nums[first] == nums[first-1]){
+                continue;
+            }
+
+            int second = first + 1, third = nums.length-1;
+            while(second < third){
+                int tmp = nums[first] + nums[second] + nums[third];
+                if(tmp == 0){
+                    results.add(Arrays.asList(nums[first], nums[second], nums[third]));
+                    do{
+                        second++;
+                    }while(second < third && nums[second-1] == nums[second]);
+
+                    do{
+                        third--;
+                    }while(second < third && nums[third+1] == nums[third]);
+                }else if(tmp > 0){
+                    third--;
+                }else{
+                    second++;
+                }
+            }
+        }
+
+        return results;
+    }
+}
---
+++ src/test/java/cn/xhh/leetcode/_015/Sum3Test.java
@@ -0,0 +1,129 @@
+package cn.xhh.leetcode._015;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+
+/**
+ * Sum3Test
+ *
+ * @author <a href="mailto:bigtiger02@gmail.com">xhh</a>
+ * @date 2020/11/15
+ */
+public class Sum3Test {
+
+    private Solution solution = new Solution();
+
+    private void assertEquals(int[][] expects, List<List<Integer>> result){
+        boolean flag = true;
+        for (int i = 0; i < expects.length; i++) {
+            for (int j = 0; j < expects[i].length; j++) {
+                if(expects[i][j] != result.get(i).get(j)){
+                    flag = false;
+                    break;
+                }
+            }
+            if(!flag){
+                break;
+            }
+        }
+
+        Assert.assertEquals(true, flag);
+    }
+
+    @Test
+    public void case1(){
+        int[] nums = {-1, 0, 1, 2, -1, -4};
+        List<List<Integer>> result = solution.threeSum(nums);
+
+        int[][] expects = {
+            {-1,-1,2},
+            {-1,0,1}
+        };
+        assertEquals(expects, result);
+    }
+
+    @Test
+    public void case2(){
+        int[] nums = {};
+        List<List<Integer>> result = solution.threeSum(nums);
+
+        int[][] expects = {};
+        assertEquals(expects, result);
+    }
+
+    @Test
+    public void case3(){
+        int[] nums = {1};
+        List<List<Integer>> result = solution.threeSum(nums);
+
+        int[][] expects = {};
+        assertEquals(expects, result);
+    }
+
+    @Test
+    public void case4(){
+        int[] nums = {1,2};
+        List<List<Integer>> result = solution.threeSum(nums);
+
+        int[][] expects = {};
+        assertEquals(expects, result);
+    }
+
+    @Test
+    public void case5(){
+        int[] nums = {1,2,3};
+        List<List<Integer>> result = solution.threeSum(nums);
+
+        int[][] expects = {};
+        assertEquals(expects, result);
+    }
+
+    @Test
+    public void case6(){
+        int[] nums = {1,0,-1};
+        List<List<Integer>> result = solution.threeSum(nums);
+
+        int[][] expects = {
+                {-1,0,1}
+        };
+        assertEquals(expects, result);
+    }
+
+    @Test
+    public void case7(){
+        int[] nums = {0,0,0};
+        List<List<Integer>> result = solution.threeSum(nums);
+
+        int[][] expects = {
+            {0,0,0}
+        };
+        assertEquals(expects, result);
+    }
+
+    @Test
+    public void case8(){
+        int[] nums = {1,1,0,0,0,-1,-1,2};
+        List<List<Integer>> result = solution.threeSum(nums);
+
+        int[][] expects = {
+                {-1,-1,2},
+                {-1,0,1},
+                {0,0,0}
+        };
+        assertEquals(expects, result);
+    }
+
+    @Test
+    public void case9(){
+        int[] nums = {-2,0,1,1,2};
+        List<List<Integer>> result = solution.threeSum(nums);
+
+        int[][] expects = {
+            {-2,0,2},
+            {-2,1,1}
+        };
+        assertEquals(expects, result);
+    }
+}
