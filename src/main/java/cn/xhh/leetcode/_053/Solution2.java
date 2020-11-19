628af0f2d367d18efeedf841698b8145f6fcdd7a
tiger <bigtiger02@gmail.com>
2020-11-19 17:48:20 +0800

    solution maximum subarray

--- src/main/java/cn/xhh/leetcode/_053/Solution2.java
+++ src/main/java/cn/xhh/leetcode/_053/Solution2.java
@@ -6,23 +6,17 @@ package cn.xhh.leetcode._053;
  * @author <a href="mailto:bigtiger02@gmail.com">xhh</a>
  * @date 2020/11/18
  */
-public class Solution {
+public class Solution2 {
     public int maxSubArray(int[] nums) {
         if(null == nums){
             return 0;
         }
-        int maxSum = nums[0];
-        int tmpSum = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if(tmpSum > 0){
-                tmpSum = tmpSum + nums[i];
-            }else{
-                tmpSum = nums[i];
-            }
 
+        int maxSum = nums[0], tmpSum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            tmpSum = Math.max(tmpSum+nums[i], nums[i]);
             maxSum = Math.max(tmpSum, maxSum);
         }
-
         return maxSum;
     }
 }
