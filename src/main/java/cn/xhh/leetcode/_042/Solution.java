98863b1f4286b3bf95b6e12218da1f905d279cb3
tiger <bigtiger02@gmail.com>
2020-12-10 00:46:02 +0800

    solve trap water

--- src/main/java/cn/xhh/leetcode/_042/Solution.java
+++ src/main/java/cn/xhh/leetcode/_042/Solution.java
@@ -1,5 +1,7 @@
 package cn.xhh.leetcode._042;
 
+import java.util.Arrays;
+
 /**
  * Solution
  *
@@ -9,6 +11,32 @@ package cn.xhh.leetcode._042;
 public class Solution {
 
     public int trap(int[] height) {
-        return 0;
+        if(null == height || height.length < 2){
+            return 0;
+        }
+
+        int len = height.length;
+        int[] maxLefts = new int[len];
+        Arrays.fill(maxLefts,0);
+        maxLefts[0] = height[0];
+
+        int[] maxRights = new int[len];
+        Arrays.fill(maxRights,0);
+        maxRights[len-1] = height[len-1];
+
+        for (int i = 1; i < len; i++) {
+            maxLefts[i] = Math.max(maxLefts[i-1], height[i]);
+        }
+
+        for (int i = len-2; i >= 0; i--) {
+            maxRights[i] = Math.max(maxRights[i+1], height[i]);
+        }
+
+        int sum = 0;
+        for (int i = 0; i < len; i++) {
+            sum += Math.min(maxLefts[i],maxRights[i]) - height[i];
+        }
+
+        return sum;
     }
 }
