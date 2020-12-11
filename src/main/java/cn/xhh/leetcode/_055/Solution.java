63b0a95d13825db32f6f67b55685e51ee4dc7bb2
tiger <bigtiger02@gmail.com>
2020-12-11 18:31:14 +0800

    solve jump game

--- src/main/java/cn/xhh/leetcode/_055/Solution.java
+++ src/main/java/cn/xhh/leetcode/_055/Solution.java
@@ -9,6 +9,20 @@ package cn.xhh.leetcode._055;
  */
 public class Solution {
     public boolean canJump(int[] nums) {
+        if(null == nums || nums.length == 0){
+            return true;
+        }
+
+        int rightMost = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if(i<=rightMost){
+                rightMost = Math.max(rightMost, i + nums[i]);
+                if(rightMost >= nums.length-1){
+                    return true;
+                }
+            }
+        }
+
         return false;
     }
 }
