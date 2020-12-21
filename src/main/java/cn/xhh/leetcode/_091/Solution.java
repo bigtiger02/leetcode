f7f10abad94c4f23a5fa12133b208cfe3e07af26
tiger <bigtiger02@gmail.com>
2020-12-21 20:29:55 +0800

    solve 091

--- src/main/java/cn/xhh/leetcode/_091/Solution.java
+++ src/main/java/cn/xhh/leetcode/_091/Solution.java
@@ -13,19 +13,45 @@ public class Solution {
         }
 
         int len = s.length();
-        int[] notJoin = new int[len];
-        int[] join = new int[len];
-        notJoin[0] = 1;
-        join[0] = 0;
+        int[] dp = new int[len+1];
+        dp[0] = 1;
+        dp[1] = s.charAt(0) == '0' ? 0 : 1;
 
         for (int i = 1; i < len; i++) {
             int num = s.charAt(i) - '0';
             int lastNum = s.charAt(i-1) - '0';
             int totalNum = lastNum*10 + num;
-            notJoin[i] = num == 0 ? 0 : notJoin[i-1] + join[i-1];
-            join[i] = totalNum > 26 ? 0 : notJoin[i-1];
+            //既可以与前一个凑，也可以和后一个凑
+            //只能单独使用
+            //只能和前一个凑
+            //不能凑
+            if(num > 0 && num < 7 && totalNum > 0 && totalNum <= 26){
+                dp[i+1] = dp[i] + dp[i-1];
+            }else if(totalNum == 10 || totalNum == 20 || totalNum > 26){
+                dp[i+1] = dp[i];
+            }else{
+                return 0;
+            }
+            //只能和前一个凑
+            //不能凑
+            if(num == 0){
+                if(totalNum == 10 || totalNum == 20){
+                    //还可以和前一个凑
+                    dp[i+1] = dp[i-1];
+                }else{
+                    return 0;
+                }
+            }else{
+                if(totalNum > 10 && totalNum <= 26){
+                    //表明该数字既可以单独又可以和前一个组合
+                    dp[i+1] = dp[i] + dp[i+1];
+                }else{
+                    //和前一个数字无法组合
+                    dp[i+1] = dp[i];
+                }
+            }
         }
 
-        return notJoin[len-1] + join[len-1];
+        return dp[len];
     }
 }
---
+++ src/main/java/cn/xhh/leetcode/_091/Solution1.java
@@ -0,0 +1,31 @@
+package cn.xhh.leetcode._091;
+
+/**
+ * Decode ways
+ * <a href="https://leetcode-cn.com/problems/decode-ways/"></a>
+ * @author <a href="mailto:bigtiger02@gmail.com">Decode ways</a>
+ * @date 2020/12/17
+ */
+public class Solution1 {
+    public int numDecodings(String s) {
+        if(s.charAt(0) == '0'){
+            return 0;
+        }
+
+        int len = s.length();
+        int[] notJoin = new int[len];
+        int[] join = new int[len];
+        notJoin[0] = 1;
+        join[0] = 0;
+
+        for (int i = 1; i < len; i++) {
+            int num = s.charAt(i) - '0';
+            int lastNum = s.charAt(i-1) - '0';
+            int totalNum = lastNum*10 + num;
+            notJoin[i] = num == 0 ? 0 : notJoin[i-1] + join[i-1];
+            join[i] = totalNum > 26 ? 0 : notJoin[i-1];
+        }
+
+        return notJoin[len-1] + join[len-1];
+    }
+}
