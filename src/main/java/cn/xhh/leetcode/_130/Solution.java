eb8e4fb908033c3d42cebd340bfc5e2c360cac4f
tiger <bigtiger02@gmail.com>
2020-12-29 18:11:09 +0800

    solve surrounded regions

---
+++ src/main/java/cn/xhh/leetcode/_130/Solution.java
@@ -0,0 +1,19 @@
+package cn.xhh.leetcode._130;
+
+/**
+ * surrounded-regions
+ * <a href="https://leetcode-cn.com/problems/surrounded-regions/">surrounded-regions</a>
+ * @author <a href="mailto:bigtiger02@gmail.com">xhh</a>
+ * @date 2020/12/29
+ */
+public class Solution {
+
+    public void solve(char[][] board) {
+        int len = board.length;
+        for (int i = 0; i < len; i++) {
+            for (int j = 0; j < len; j++) {
+
+            }
+        }
+    }
+}
---
+++ src/test/java/cn/xhh/leetcode/_130/SurroundedRegionsTest.java
@@ -0,0 +1,18 @@
+package cn.xhh.leetcode._130;
+
+import org.junit.Test;
+
+/**
+ * SurroundedRegionsTest
+ *
+ * @author <a href="mailto:bigtiger02@gmail.com">xhh</a>
+ * @date 2020/12/29
+ */
+public class SurroundedRegionsTest {
+    private final Solution solution = new Solution();
+
+    @Test
+    public void case1() {
+
+    }
+}
